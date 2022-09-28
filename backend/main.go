5fa190cbd20f1b1a88a62afa535b55ee76e135a8
TheaKevin <theakevin01@gmail.com>
2022-09-28 13:45:00 +0700

    buat post

--- backend/main.go
+++ backend/main.go
@@ -9,17 +9,35 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
+var db []string
+
+type DataRequest struct {
+	Text string `json:"text"`
+}
+
 func main() {
+	db = make([]string, 0)
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"*"},
 	}))
 	r.GET("/", handler)
+	r.POST("/send", postHandler)
 	r.Run(":" + os.Getenv("PORT"))
 }
 
 func handler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello World",
+		"data": db,
 	})
+}
+
+func postHandler(c *gin.Context) {
+	var data DataRequest
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db = append(db, data.Text)
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": data.Text})
 }
