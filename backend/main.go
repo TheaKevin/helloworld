b883860cd1c2d355cbfa29843e28ac20dd547744
TheaKevin <theakevin01@gmail.com>
2022-09-29 10:32:45 +0700

    feat: added database support

--- backend/main.go
+++ backend/main.go
@@ -7,51 +7,98 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-// var db []string
-
-type DB struct {
-	text string
+type Todos struct {
+	gorm.Model
+	Task string `json:"task"`
+	Done bool   `json:"done"`
 }
 
-var db []DB
+type repository struct {
+	db *gorm.DB
+}
 
 type DataRequest struct {
-	Text string `json:"text"`
+	Task string `json:"task" binding:"required"`
 }
 
 func main() {
-	// db = make([]string, 0)
+	var db *gorm.DB
+	var err error
+	dbUrl := os.Getenv("DATABASE_URL")
+
+	if os.Getenv("ENVIRONMENT") == "PROD" {
+		db, err = gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	} else {
+		db, err = gorm.Open(sqlite.Open(dbUrl), &gorm.Config{})
+	}
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic("failed to get database")
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		panic("failed to ping database")
+	}
+
+	if err := db.AutoMigrate(&Todos{}); err != nil {
+		panic("failed to migrate database")
+	}
+
+	repo := repository{
+		db: db,
+	}
+
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"*"},
 	}))
-	r.GET("/", handler)
-	r.POST("/send", postHandler)
+
+	r.GET("/", repo.handler)
+	r.POST("/send", repo.postHandler)
 	r.Run(":" + os.Getenv("PORT"))
 }
 
-func handler(c *gin.Context) {
-	var s []string
-	for _, v := range db {
-		s = append(s, v.text)
+func (r *repository) handler(c *gin.Context) {
+	var todos []Todos
+	res := r.db.Find(&todos)
+	if res.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": res.Error.Error()})
+		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"data": s,
+		"data": todos,
 	})
 }
 
-func postHandler(c *gin.Context) {
+func (r *repository) postHandler(c *gin.Context) {
 	var data DataRequest
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// db = append(db, data.Text)
-	var dataBaru = DB{text: data.Text}
 
-	db = append(db, dataBaru)
+	todo := Todos{
+		Task: data.Task,
+		Done: false,
+	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": data.Text})
+	res := r.db.Create(&todo)
+	if res.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": res.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": todo})
 }
